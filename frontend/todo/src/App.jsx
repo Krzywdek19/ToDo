57e963e4fb1d4c33c162fe224237befb504bd6b9
SculptTechProject <sculpttechproject@gmail.com>
2024-07-11 20:20:00 +0200

    initial commit

---
+++ frontend/todo/src/App.jsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import HeaderText from "./components/HeaderText";
+
+function App() {
+  return (
+    <>
+      <HeaderText/>
+    </>
+  );
+}
+
+export default App;
