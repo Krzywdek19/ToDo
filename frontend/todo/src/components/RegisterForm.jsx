3d922e4f3b9aa72b2d433214d19639974097e60f
SculptTechProject <sculpttechproject@gmail.com>
2024-07-20 22:13:54 +0200

    success screen for register to sign up

--- frontend/todo/src/components/RegisterForm.jsx
+++ frontend/todo/src/components/RegisterForm.jsx
@@ -76,179 +76,196 @@ const RegisterForm = () => {
     setSuccess(true);
   };
 
-
-
   return (
-    <div className="registerForm">
-      <div className="card cardRegister">
-        <div className="card-body">
-          <div className="bodyHeader">
-            <p className="registerHeader">Register</p>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="48"
-              height="48"
-              fill="#595959"
-              className="bi bi-caret-left-fill"
-              viewBox="0 0 16 16"
-            >
-              <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z" />
-            </svg>
-          </div>
-          <hr className="headerLine"></hr>
-          {/* Form */}
-          <section>
-            <p
-              ref={errRef}
-              className={errMsg ? "errmsg" : "offscreen"}
-              aria-live="assertive"
-            >
-              {errMsg}
-            </p>
+    <>
+      {success ? (
+        <sectiom className="successText">
+          <h1>Success!</h1>
+          <p>Please Sign in!</p>
+        </sectiom>
+      ) : (
+        <div className="registerForm">
+          <div className="card cardRegister">
+            <div className="card-body">
+              <div className="bodyHeader">
+                <p className="registerHeader">Register</p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="48"
+                  height="48"
+                  fill="#595959"
+                  className="bi bi-caret-left-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z" />
+                </svg>
+              </div>
+              <hr className="headerLine"></hr>
+              {/* Form */}
+              <section>
+                <p
+                  ref={errRef}
+                  className={errMsg ? "errmsg" : "offscreen"}
+                  aria-live="assertive"
+                >
+                  {errMsg}
+                </p>
 
-            <form
-              className="formRegister pt-2" onSubmit={handleSubmitRegister}
-            >
-              {/* ------ USERNAME ------*/}
-              <div className="formUsernameRegister py-2">
-                <label
-                  className="usernameTextRegister fs-4 fw-semibold"
-                  htmlFor="username"
+                <form
+                  className="formRegister pt-2"
+                  onSubmit={handleSubmitRegister}
                 >
-                  Username: &nbsp;
-                  <span className={validName ? "valid" : "hide"}>
-                    <FontAwesomeIcon icon={faCheck} />
-                  </span>
-                  <span className={validName || !user ? "hide" : "invalid"}>
-                    <FontAwesomeIcon icon={faTimes} />
-                  </span>
-                </label>
-                <br />
-                <input
-                  type="text"
-                  id="username"
-                  ref={userRef}
-                  autoComplete="off"
-                  required
-                  aria-invalid={validName ? "false" : "true"}
-                  aria-describedby="uidnote"
-                  onChange={(e) => setUser(e.target.value)}
-                  placeholder="Enter your username"
-                  className="formInputRegister"
-                  onFocus={() => setUserFocus(true)}
-                  onBlur={() => setUserFocus(false)}
-                />
-                {/* Checks, if the username is valid with REGEX and giving instructions*/}
-                <p
-                  id="uidnote"
-                  className={
-                    userFocus && !validName && user
-                      ? "instructions"
-                      : "offscreen"
-                  }
-                >
-                  <FontAwesomeIcon icon={faInfoCircle} />
-                  &nbsp;4 to 24 character.
-                  <br />
-                  Must begin with a letter.
-                  <br />
-                  Letters, numbers, underscores, hyphens allowed.
-                </p>
-              </div>
-              {/* ------ PASSWORD ------ */}
-              <div className="formPasswordRegister">
-                <label
-                  className="passwordTextRegister fs-4 fw-semibold"
-                  htmlFor="password"
-                >
-                  Password:&nbsp;
-                  <span className={validPwd ? "valid" : "hide"}>
-                    <FontAwesomeIcon icon={faCheck} />
-                  </span>
-                  <span className={validPwd || !pwd ? "hide" : "invalid"}>
-                    <FontAwesomeIcon icon={faTimes} />
-                  </span>
-                </label>
-                <br />
-                <input
-                  type="password"
-                  id="password"
-                  required
-                  aria-invalid={validPwd ? "false" : "true"}
-                  aria-describedby="pwdnote"
-                  onFocus={() => setPwdFocus(true)}
-                  onBlur={() => setPwdFocus(false)}
-                  onChange={(e) => setPwd(e.target.value)}
-                  placeholder="Enter your password"
-                  className="formInputRegister"
-                />
-                <p
-                  id="pwdnote"
-                  className={
-                    pwdFocus && !validPwd ? "instructions" : "offscreen"
-                  }
-                >
-                  <FontAwesomeIcon icon={faInfoCircle} />
-                  &nbsp;8 to 24 character.
-                  <br />
-                  Must include uppercase and lowercase letters,
-                  <br /> a numer and a special character.
-                  <br />
-                  Allowed special characters:{" "}
-                  <span aria-label="exclamation mark">!</span>
-                  <span aria-label="at symbol">@</span>{" "}
-                  <span aria-label="hashtag">#</span>
-                  <span aria-label="percent">%</span>
-                </p>
-              </div>
-              {/* ------ CONFIRM PASSWORD ------ */}
-              <div className="formPasswordRegister">
-                <label
-                  className="passwordTextRegister fs-4 fw-semibold"
-                  htmlFor="confirm_pwd"
-                >
-                  Confirm Password:&nbsp;
-                  <span className={validMatch && matchPwd ? "valid" : "hide"}>
-                    <FontAwesomeIcon icon={faCheck} />
-                  </span>
-                  <span
-                    className={validMatch || !matchPwd ? "hide" : "invalid"}
-                  >
-                    <FontAwesomeIcon icon={faTimes} />
-                  </span>
-                </label>
-                <br />
-                <input
-                  type="password"
-                  id="confirm_pwd"
-                  required
-                  aria-invalid={validMatch ? "false" : "true"}
-                  aria-describedby="confirmnote"
-                  onFocus={() => setMatchFocus(true)}
-                  onBlur={() => setMatchFocus(false)}
-                  onChange={(e) => setMatchPwd(e.target.value)}
-                  placeholder="Enter your password"
-                  className="formInputRegister"
-                />
-                <p
-                  id="confirmnote"
-                  className={
-                    matchFocus && !validMatch ? "instructions" : "offscreen"
-                  }
-                >
-                  <FontAwesomeIcon icon={faInfoCircle} />
-                  &nbsp;Must match the first password input field.
-                </p>
-              </div>
-              {/* SUBMIT */}
-              <div className="formBtnRegister">
-                <button className="registerBtn fw-semibold" disabled={!validName || !validPwd || !validMatch ? true : false}>Sign up</button>
-              </div>
-            </form>
-          </section>
+                  {/* ------ USERNAME ------*/}
+                  <div className="formUsernameRegister py-2">
+                    <label
+                      className="usernameTextRegister fs-4 fw-semibold"
+                      htmlFor="username"
+                    >
+                      Username: &nbsp;
+                      <span className={validName ? "valid" : "hide"}>
+                        <FontAwesomeIcon icon={faCheck} />
+                      </span>
+                      <span className={validName || !user ? "hide" : "invalid"}>
+                        <FontAwesomeIcon icon={faTimes} />
+                      </span>
+                    </label>
+                    <br />
+                    <input
+                      type="text"
+                      id="username"
+                      ref={userRef}
+                      autoComplete="off"
+                      required
+                      aria-invalid={validName ? "false" : "true"}
+                      aria-describedby="uidnote"
+                      onChange={(e) => setUser(e.target.value)}
+                      placeholder="Enter your username"
+                      className="formInputRegister"
+                      onFocus={() => setUserFocus(true)}
+                      onBlur={() => setUserFocus(false)}
+                    />
+                    {/* Checks, if the username is valid with REGEX and giving instructions*/}
+                    <p
+                      id="uidnote"
+                      className={
+                        userFocus && !validName && user
+                          ? "instructions"
+                          : "offscreen"
+                      }
+                    >
+                      <FontAwesomeIcon icon={faInfoCircle} />
+                      &nbsp;4 to 24 character.
+                      <br />
+                      Must begin with a letter.
+                      <br />
+                      Letters, numbers, underscores, hyphens allowed.
+                    </p>
+                  </div>
+                  {/* ------ PASSWORD ------ */}
+                  <div className="formPasswordRegister">
+                    <label
+                      className="passwordTextRegister fs-4 fw-semibold"
+                      htmlFor="password"
+                    >
+                      Password:&nbsp;
+                      <span className={validPwd ? "valid" : "hide"}>
+                        <FontAwesomeIcon icon={faCheck} />
+                      </span>
+                      <span className={validPwd || !pwd ? "hide" : "invalid"}>
+                        <FontAwesomeIcon icon={faTimes} />
+                      </span>
+                    </label>
+                    <br />
+                    <input
+                      type="password"
+                      id="password"
+                      required
+                      aria-invalid={validPwd ? "false" : "true"}
+                      aria-describedby="pwdnote"
+                      onFocus={() => setPwdFocus(true)}
+                      onBlur={() => setPwdFocus(false)}
+                      onChange={(e) => setPwd(e.target.value)}
+                      placeholder="Enter your password"
+                      className="formInputRegister"
+                    />
+                    <p
+                      id="pwdnote"
+                      className={
+                        pwdFocus && !validPwd ? "instructions" : "offscreen"
+                      }
+                    >
+                      <FontAwesomeIcon icon={faInfoCircle} />
+                      &nbsp;8 to 24 character.
+                      <br />
+                      Must include uppercase and lowercase letters,
+                      <br /> a numer and a special character.
+                      <br />
+                      Allowed special characters:{" "}
+                      <span aria-label="exclamation mark">!</span>
+                      <span aria-label="at symbol">@</span>{" "}
+                      <span aria-label="hashtag">#</span>
+                      <span aria-label="percent">%</span>
+                    </p>
+                  </div>
+                  {/* ------ CONFIRM PASSWORD ------ */}
+                  <div className="formPasswordRegister">
+                    <label
+                      className="passwordTextRegister fs-4 fw-semibold"
+                      htmlFor="confirm_pwd"
+                    >
+                      Confirm Password:&nbsp;
+                      <span
+                        className={validMatch && matchPwd ? "valid" : "hide"}
+                      >
+                        <FontAwesomeIcon icon={faCheck} />
+                      </span>
+                      <span
+                        className={validMatch || !matchPwd ? "hide" : "invalid"}
+                      >
+                        <FontAwesomeIcon icon={faTimes} />
+                      </span>
+                    </label>
+                    <br />
+                    <input
+                      type="password"
+                      id="confirm_pwd"
+                      required
+                      aria-invalid={validMatch ? "false" : "true"}
+                      aria-describedby="confirmnote"
+                      onFocus={() => setMatchFocus(true)}
+                      onBlur={() => setMatchFocus(false)}
+                      onChange={(e) => setMatchPwd(e.target.value)}
+                      placeholder="Enter your password"
+                      className="formInputRegister"
+                    />
+                    <p
+                      id="confirmnote"
+                      className={
+                        matchFocus && !validMatch ? "instructions" : "offscreen"
+                      }
+                    >
+                      <FontAwesomeIcon icon={faInfoCircle} />
+                      &nbsp;Must match the first password input field.
+                    </p>
+                  </div>
+                  {/* SUBMIT */}
+                  <div className="formBtnRegister">
+                    <button
+                      className="registerBtn fw-semibold"
+                      disabled={
+                        !validName || !validPwd || !validMatch ? true : false
+                      }
+                    >
+                      Sign up
+                    </button>
+                  </div>
+                </form>
+              </section>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
